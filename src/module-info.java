73a5d5828d620f4002229f20c24cff9c1ec02c15
davidmozart <davidmozart01@gmail.com>
2021-09-14 14:59:12 -0300

    Main view

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,10 @@
 module projetoJavaFxJdbc {
 	requires javafx.controls;
-	
-	/*
-	 * exports gui; opens gui to javafx.fxml;
-	 */
-	
+	requires javafx.fxml;
+
+//	exports gui; 
+//	opens gui to javafx.fxml;
+	 
+
 	opens application to javafx.graphics, javafx.fxml;
 }
